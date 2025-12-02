20432ffc0ef1c2a1fb68cc477436db2b2d5bb0f6
jparedesa-eng <jparedesa@danper.com>
2025-12-02 12:50:58 -0500

    se quito configuracion supabase

--- src/supabase/supabase.ts
+++ src/supabase/supabase.ts
@@ -6,7 +6,6 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[]
 
-export { supabase } from "./supabaseClient";
 export type Database = {
   // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
